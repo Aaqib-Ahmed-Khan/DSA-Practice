c1c3dc9441ade016ade3ead8889c41b8b510d6ec
AAQIB AHMED KHAN <aaqibahmed365@gmail.com>
2025-08-09 23:53:59 +0500

    for each loop

--- Vectors/loop_in_vector.cpp
+++ Vectors/loop_in_vector.cpp
@@ -1,18 +1,49 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main(){
+// vector<int> v(5);
+
+// for (int i=0;i<5;i++){
+// int element;
+// cin>>element;
+// v.push_back(element);
+// //   cin>>v[i];
+
+// }
+// for(int i=0;i<=5;i++){
+//     cout<<v[i]<<" ";
+
+// }
+
+// cout<<endl;
+// // cout<<aaqib<<endl;
+// }
 #include <iostream>
 #include <vector>
 using namespace std;
-int main(){
-vector<int> v(5);
 
-for (int i=0;i<5;i++){
-int element;
-cin>>element;
-v.push_back(element);
-//   cin>>v[i];
+int main() {
+    vector<int> v; // start empty
 
+    for (int i = 0; i < 5; i++) {
+        int element;
+        cin >> element;
+        v.push_back(element);
+    }
+
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+
+// for each loop
+    for (int ele : v) {
+    cout << ele << " ";
 }
-for(int i=0;i<=5;i++){
-    cout<<v[i]<<" ";
+cout << endl;
+
+    return 0;
 }
-cout<<endl;
-}
+
