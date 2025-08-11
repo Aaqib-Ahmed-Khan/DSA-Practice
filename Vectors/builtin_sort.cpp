7a82199d4f174d24c04355e8da13e0f998afcf2a
AAQIB AHMED KHAN <aaqibahmed365@gmail.com>
2025-08-11 22:27:04 +0500

    two sum question

--- Vectors/builtin_sort.cpp
+++ Vectors/builtin_sort.cpp
@@ -18,3 +18,26 @@ for (int i=0;i<v.size();i++){
     cout<<v[i]<<" ";
 }
 return 0;}
+
+
+// Q:two sum :Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        vector<int> v(2);
+        int n = arr.size();
+        for (int i=0;i < n;i++){
+            for (int j = i+1; j < n; j++){
+                if(arr[i]+arr[j]==target){
+                    v[0]= i;
+                    v[1]= j;
+                    return v;
+                }
+            }
+        }   return v; }
+};
