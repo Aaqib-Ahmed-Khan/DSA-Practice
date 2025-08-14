1120f903b436939fbf5f78da6b267b4b6f051e32
AAQIB AHMED KHAN <aaqibahmed365@gmail.com>
2025-08-14 20:48:42 +0500

    mastering arrays

---
+++ Arrays/Mastering_arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int arr[5]={1,2,4,5,6};
+int x=5;
+int n=sizeof(arr)/sizeof(arr[0]);
+for(int i=0;i<n;i++){
+    if(arr[i]==x){
+    cout<<"found at index"<< i<<endl;
+    return 0;
+    }
+}
+cout<<"not found";
+return 0;  
+}
+
