45d1fb3d2116d1acf6175a50e530cd3ed065c0fe
AAQIB AHMED KHAN <aaqibahmed365@gmail.com>
2025-08-17 13:10:06 +0500

    rotation array

--- Arrays/rotation.cpp
+++ Arrays/rotation.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
-int main(){
-
+void rotate(int arr[],int n){
+    int last =arr[n-1];
+    for(int i=n-2;i>=0;i--){
+        arr[i+1]=arr[i];
+    }
+    arr[0]=last;
 }
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+
+ rotate( arr, n);
+    cout <<"array after rotation: ";
+
+for(int i=0;i<n;i++){
+        cout<<arr[i]<<"";
+    }cout<<endl;
+
+return 0;}
