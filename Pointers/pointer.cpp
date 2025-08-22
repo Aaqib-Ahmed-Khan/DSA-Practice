d43613e8e48a29020d12d6d3076c1d04b1d9a82c
Aaqib Ahmed Khan <aaqibahmed365@gmail.com>
2025-08-22 21:02:56 +0500

    basic  practice of pointers

--- Pointers/pointer.cpp
+++ Pointers/pointer.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-
+int a=10;
+cout<<&a<<endl;
+int *ptr=&a;
+cout<<sizeof(ptr)<<endl;
+float m= 2.4;
+float *ptr1 =&m;
+cout<<sizeof(ptr1)<<endl;;
+return 0;
 }
