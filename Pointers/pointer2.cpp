09afe94b884281194ce8682379b848525f60fdf2
Aaqib Ahmed Khan <aaqibahmed365@gmail.com>
2025-09-09 20:01:43 +0500

    pointers practice

---
+++ Pointers/pointer2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5]={11,2,3,4,5};
+cout<<arr<<endl;
+int *ptr=arr;
+    cout<<&arr[0]<<endl;
+    cout<<ptr<<endl;
+
+
+
+    return 0;}
