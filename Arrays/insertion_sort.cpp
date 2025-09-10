dd36ad5621bc1a645c409880ed7e01fca46afe79
Aaqib Ahmed Khan <aaqibahmed365@gmail.com>
2025-09-10 21:31:23 +0500

    insertion sort practicing by some other method

--- Arrays/insertion_sort.cpp
+++ Arrays/insertion_sort.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 using namespace std;
-int main(){
-int arr[1000];
-int n;
-cout<<"enter size of array";
-cin>>n;
-for(int i=0;i<n;i++)
-cin>>arr[i];
-for (int i=1;i<n;i++)
-{
-    for (int j=i;j>0;j--)
-    {
-        if(arr[j]<arr[j-1])
-        swap(arr[j],arr[j-1]);
-        else
-        break;
+int main() {
+    // int arr[1000];
+    // int n;
+    // cout<<"enter size of array";
+    // cin>>n;
+    // for(int i=0;i<n;i++)
+    // cin>>arr[i];
+    // for (int i=1;i<n;i++)
+    // {
+    //     for (int j=i;j>0;j--)
+    //     {
+    //         if(arr[j]<arr[j-1])
+    //         swap(arr[j],arr[j-1]);
+    //         else
+    //         break;
+    //     }
+    // }
+    // for(int i=0;i<n;i++)
+    // cout<<arr[i]<<" ";
+    //
+    // }
+
+    // practice 2
+int a[5]={1,4,3,6,6};
+    int n = sizeof(a) / sizeof(a[0]);
+    for (int i=0;i<a;i++) {
+        int temp=a[i];
+        int j=i-1;
+        while (j>=0&&a[j]>temp) {
+            a[j+1]=a[j];
+            j--;
+
+        }
+        a[j+1]=temp;
     }
-}
-for(int i=0;i<n;i++)
-cout<<arr[i]<<" ";
+
 }
