e71d7b5ec01663adaef7fe280a8d60262eb23386
Aaqib Ahmed Khan <aaqibahmed365@gmail.com>
2025-09-10 21:33:33 +0500

    fixed some errors

--- Arrays/insertion_sort.cpp
+++ Arrays/insertion_sort.cpp
@@ -25,15 +25,20 @@ int main() {
     // practice 2
 int a[5]={1,4,3,6,6};
     int n = sizeof(a) / sizeof(a[0]);
-    for (int i=0;i<a;i++) {
+    for (int i=1;i<n;i++) {
         int temp=a[i];
         int j=i-1;
-        while (j>=0&&a[j]>temp) {
+        while (j>=0 && a[j]>temp) {
             a[j+1]=a[j];
             j--;
 
         }
         a[j+1]=temp;
     }
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
 
+    return 0;
 }
