bcf4222d001c521d92e9dc63cc199345bff02ebd
Aaqib Ahmed Khan <aaqibahmed365@gmail.com>
2025-09-11 20:17:23 +0500

    selection sort

---
+++ Arrays/selection.cpp
@@ -0,0 +1,19 @@
+//
+// Created by aaqib on 9/11/25.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    int a[5]={1,4,3,2,5};
+    int n=5;
+    for (int i=0;i<n;i++) {
+        int min=i;
+        for (int j=i+1;j<n;j++)
+            if (a[j]<a[min]){
+        min=j;
+    }
+    if (min !=i) {
+        swap(a[i],a[min]);
+
+}}
+
