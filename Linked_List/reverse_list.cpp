a9b510af66c88fe3fc8cce65b29b880e67141f0e
Aaqib Ahmed Khan <aaqibahmed365@gmail.com>
2025-09-16 20:01:29 +0500

    reverse linked list

--- Linked_List/reverse_list.cpp
+++ Linked_List/reverse_list.cpp
@@ -1,16 +1,40 @@
-//
-// Created by aaqib on 9/16/25.
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
 
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode *curr = head, *prev = NULL, *fut = NULL;
-        while (curr != NULL) {  // ✅ FIXED condition
-            fut = curr->next;   // store next node
-            curr->next = prev;  // reverse pointer
-            prev = curr;        // move prev forward
-            curr = fut;         // move curr forward
+        ListNode *prev = nullptr, *curr = head;
+        while (curr) {
+            ListNode* fut = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = fut;
         }
-        return prev;  // new head
+        return prev;
     }
 };
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    Solution sol;
+    ListNode* rev = sol.reverseList(head);
+
+    // Print reversed list
+    for (ListNode* p = rev; p; p = p->next)
+        cout << p->val << (p->next ? " -> " : "\n");
+
+    return 0;
+}
+
