687a88bcea5613113758e3579c7ddd2b8820a830
Aaqib Ahmed Khan <aaqibahmed365@gmail.com>
2025-09-20 11:07:07 +0500

    some changes with code

--- Stack/Stackusing_arrays.cpp
+++ Stack/Stackusing_arrays.cpp
@@ -1,77 +1,70 @@
 #include <iostream>
 #include <climits>
 using namespace std;
-class Stack{
-int capacity;
-int* arr;
-int top;
+class Stack {
+    int capacity;
+    int* arr;
+    int top;
     int size;
 public:
-Stack(int s){
-   size=s;
-    top=-1;
-    arr=new int(s);
-}
+    Stack(int s){
+        size=s;
+        top=-1;
+        arr=new int[s];
+    }
     // push
-void push(int value){
-    if(top==size-1) {
-        cout<<"Stack overflow";
+    void push(int value) {
+        if(top==size-1) {
+            cout<<"Stack overflow";
 
-        return;
+            return;
+        }
+        else {
+            top++;
+            arr[top]=value;
+            cout<<"Pushed"<<value<<"into the stack\n";
+        }
     }
-    else {
-        top++;
-        arr[top]=value;
-        cout<<"Pushed"<<value<<"into the stack\n";
+    int pop() {
+        if (top == -1) {
+            cout << "Stack underflow\n";
+            return -1;
+        }
+
+        else {
+            cout << "Popped " << arr[top] << " from the stack\n";
+
+            top--;
+            return 0;
+        }
     }
-    this->top++;
-    this->arr[this->top]=data;}
-int pop() {
-    if (this->top == -1) {
-        cout << "Underflow\n";
-        return INT_MIN;
+    int peek() {
+        if (top == -1) {
+            cout << "Stack underflow (empty stack)\n";
+            return -1;
+        }
+        else {
+            return arr[top];
+        }
+
     }
-    int value = this->arr[this->top]; 
-    this->top--;                      
-    return value;                     
-}
-
-
-int getTop(){
-    if(this->top ==-1){
-        cout<<"underflow\n";
-        return INT_MIN;
+    bool isEmpty() {
+        return top==-1;
     }
-return this->arr[this->top];
-
-}
-
-bool isEmpty(){
-    return this->top == -1;
-
-}
-bool isFull(){
-    return this->top ==this->capacity -1;
-}
-
-   
+    int isSize() {
+        return top+1;
+    }
 };
+    int main() {
+    Stack st(5);
 
-int main(){
-  Stack st(5);
-    st.push(1);
-    cout << st.getTop() << "\n";  // prints 1
+    st.push(10);
+    st.push(20);
 
-    st.push(2);
-    st.push(3);
-    cout << st.getTop() << "\n";  // prints 3
+    cout << "Peek: " << st.peek() << endl;
 
-    st.push(4); 
-    st.push(5);
-    cout << st.getTop() << "\n";  // prints 5
-
-    st.push(8); // should trigger Overflow
-    st.push(9);
+    st.pop();
+    cout << "Peek: " << st.peek() << endl;
 
     return 0;
 }
