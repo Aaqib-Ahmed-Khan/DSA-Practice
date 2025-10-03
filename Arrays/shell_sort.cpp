de6763a9857847b53add6562d8b6d1462ca55343
Aaqib Ahmed Khan <aaqibahmed365@gmail.com>
2025-10-03 21:40:02 +0500

    Shell sort done

---
+++ Arrays/shell_sort.cpp
@@ -0,0 +1,41 @@
+//
+// Created by aaqib on 10/3/25.
+//
+
+#include <iostream>
+using namespace std;
+
+void shellSort(int arr[], int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {23, 12, 1, 8, 34, 54, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Before sorting: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    shellSort(arr, n);
+
+    cout << "After sorting: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
