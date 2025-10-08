3fea8c7b323172fc10519e2a3ebe34dc69c156d5
Aaqib Ahmed Khan <aaqibahmed365@gmail.com>
2025-10-08 21:22:30 +0500

    Created a linked list from an array by appending nodes to the tail. Includes logic to print the list contents.

--- Linked_List/Q_linked list.cpp
+++ Linked_List/Q_linked list.cpp
@@ -1,8 +1,42 @@
 //
 // Created by aaqib on 10/8/25.
 //
+
+ // Using a Node class, create a singly linked list where each new node is added at the end (tail).
 #include <iostream>
 using namespace std;
-int main(){
-Node
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+int main() {
+    Node* Head = nullptr;
+    Node* Tail = nullptr;
+    int arr[]={2,4,6,8,10};
+    for (int i=0;i<5;i++) {
+        if (Head==nullptr) {
+            Head=new Node(arr[i]);
+            Tail=Head;
+        }
+
+    else {
+        Tail->next=new Node(arr[i]);
+        Tail=Tail->next;
+    }
+}
+Node* temp=Head;
+
+while (temp){
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    }
+
+cout <<endl;
+return 0;
 }
